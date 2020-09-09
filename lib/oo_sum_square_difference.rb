f0f83b365caf185b73e386e82fe92e4e80e68ec6
jequezinho <jroverby92@gmail.com>
2020-09-09 13:32:56 -0500

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,9 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+    def initialize(n)
+        @n = n
+    end
+
+    def difference
+        (1..@n).sum ** 2 - (1..@n).inject(0) { |sum, i| sum + i ** 2}
+    end
+end
